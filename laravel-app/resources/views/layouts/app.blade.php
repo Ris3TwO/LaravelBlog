2c53fbdbce0f9d96a3417275c403653ce2942205
Ris3TwO <ris3two@gmail.com>
2019-09-08 21:56:47 -0300

    Fix assets

--- laravel-app/resources/views/layouts/app.blade.php
+++ laravel-app/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     ================================================== -->
     <meta charset="utf-8">
 	<title>@yield('meta-title', config('app.name') . " | Blog")</title>
-	<meta name="description" content="@yield('meta-description', 'Este es el blog de Yaremí Mendoza')">  
+	<meta name="description" content="@yield('meta-description', 'Este es el blog de Yaremí Mendoza')">
 	<meta name="author" content="">
 
     <!-- mobile specific metas
@@ -14,16 +14,16 @@
 
  	<!-- CSS
     ================================================== -->
-    <link href="{{ asset('css/base.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/base.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/vendor.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/main.css') }}" rel="stylesheet">
     @stack('styles')
 
     <!-- script
     ================================================== -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/modernizr.js') }}"></script>
-    <script src="{{ asset('js/pace.min.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/modernizr.js') }}"></script>
+    <script src="{{ secure_asset('js/pace.min.js') }}"></script>
     @stack('scripts')
 
     <!-- favicons
@@ -33,18 +33,18 @@
 
 </head>
 <body id="top">
-    <!-- header 
+    <!-- header
     ================================================== -->
-    <header class="short-header">       
-        <div class="gradient-block"></div>	 
+    <header class="short-header">
+        <div class="gradient-block"></div>
         <div class="row header-content">
             <div class="logo">
                 <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
             </div>
-         
+
             @include('partials.nav')
-         
-            <div class="search-wrap">      
+
+            <div class="search-wrap">
                 <form role="search" method="get" class="search-form" action="#">
                     <label>
                         <span class="hide-content">Search for:</span>
@@ -53,13 +53,13 @@
                     <input type="submit" class="search-submit" value="Search">
                 </form>
                 <a href="#" id="close-search" class="close-btn">Close</a>
-            </div> <!-- end search wrap -->	
-         
+            </div> <!-- end search wrap -->
+
             <div class="triggers">
                 <a class="search-trigger" href="#"><i class="fa fa-search"></i></a>
                 <a class="menu-toggle" href="#"><span>Menu</span></a>
-            </div> <!-- end triggers -->	    
-        </div>     		       
+            </div> <!-- end triggers -->
+        </div>
     </header> <!-- end header -->
     <!-- masonry
         ================================================== -->
@@ -67,25 +67,25 @@
     <!-- footer
         ================================================== -->
          <footer>
-     
+
             <div class="footer-main">
-     
-                <div class="row">  
-     
-                   <div class="col-four tab-full mob-full footer-info">            
-     
+
+                <div class="row">
+
+                   <div class="col-four tab-full mob-full footer-info">
+
                      <h4>About Our Site</h4>
-     
+
                         <p>
                            Lorem ipsum Ut velit dolor Ut labore id fugiat in ut fugiat nostrud qui in dolore commodo eu magna Duis cillum dolor officia esse mollit proident Excepteur exercitation nulla. Lorem ipsum In reprehenderit commodo aliqua irure labore.
                            </p>
-     
+
                    </div> <!-- end footer-info -->
-     
+
                    <div class="col-two tab-1-3 mob-1-2 site-links">
-     
+
                        <h4>Site Links</h4>
-     
+
                        <ul>
                            <li><a href="#">About Us</a></li>
                              <li><a href="#">Blog</a></li>
@@ -93,13 +93,13 @@
                              <li><a href="#">Terms</a></li>
                              <li><a href="#">Privacy Policy</a></li>
                          </ul>
-     
-                   </div> <!-- end site-links -->  
-     
+
+                   </div> <!-- end site-links -->
+
                    <div class="col-two tab-1-3 mob-1-2 social-links">
-     
+
                        <h4>Social</h4>
-     
+
                        <ul>
                            <li><a href="#">Twitter</a></li>
                              <li><a href="#">Facebook</a></li>
@@ -107,63 +107,63 @@
                              <li><a href="#">Google+</a></li>
                              <li><a href="#">Instagram</a></li>
                          </ul>
-                                  
-                   </div> <!-- end social links --> 
-     
+
+                   </div> <!-- end social links -->
+
                    <div class="col-four tab-1-3 mob-full footer-subscribe">
-     
+
                        <h4>Subscribe</h4>
-     
+
                        <p>Keep yourself updated. Subscribe to our newsletter.</p>
-     
+
                        <div class="subscribe-form">
-                   
+
                            <form id="mc-form" class="group" novalidate="true">
-     
-                                 <input type="email" value="" name="dEmail" class="email" id="mc-email" placeholder="Type &amp; press enter" required=""> 
-                    
+
+                                 <input type="email" value="" name="dEmail" class="email" id="mc-email" placeholder="Type &amp; press enter" required="">
+
                                 <input type="submit" name="subscribe" >
-                    
+
                                 <label for="mc-email" class="subscribe-message"></label>
-                 
+
                              </form>
-     
-                       </div>	      		
-                                  
-                   </div> <!-- end subscribe -->         
-     
+
+                       </div>
+
+                   </div> <!-- end subscribe -->
+
                </div> <!-- end row -->
-     
+
             </div> <!-- end footer-main -->
-     
+
          <div class="footer-bottom">
                <div class="row">
-     
+
                    <div class="col-twelve">
                        <div class="copyright">
-                          <span><script> var f = new Date(); document.write(f.getFullYear()); </script> © Copyright {{ config('app.name') }} - Todos los derechos reservados</span> 
+                          <span><script> var f = new Date(); document.write(f.getFullYear()); </script> © Copyright {{ config('app.name') }} - Todos los derechos reservados</span>
                           <span>Diseño por <a href="http://www.styleshout.com/" target="_blank">Styleshout</a></span>
-                          <span>Modificado y Programado por <a href="https://www.twitter.com/xfreeshot" target="_blank">Ris3TwO</a>.</span>		         	
+                          <span>Modificado y Programado por <a href="https://www.twitter.com/xfreeshot" target="_blank">Ris3TwO</a>.</span>
                       </div>
-     
+
                       <div id="go-top">
                          <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon icon-arrow-up"></i></a>
-                      </div>         
+                      </div>
                    </div>
-     
-               </div> 
-         </div> <!-- end footer-bottom -->  
-     
-        </footer>  
-     
-        <div id="preloader"> 
+
+               </div>
+         </div> <!-- end footer-bottom -->
+
+        </footer>
+
+        <div id="preloader">
              <div id="loader"></div>
-        </div> 
-     
+        </div>
+
         <!-- Java Script
-        ================================================== --> 
-        <script src="{{ asset('js/jquery-2.1.3.min.js') }}"></script>
-        <script src="{{ asset('js/plugins.js') }}"></script>
-        <script src="{{ asset('js/main.js') }}"></script>     
+        ================================================== -->
+        <script src="{{ secure_asset('js/jquery-2.1.3.min.js') }}"></script>
+        <script src="{{ secure_asset('js/plugins.js') }}"></script>
+        <script src="{{ secure_asset('js/main.js') }}"></script>
 </body>
 </html>
