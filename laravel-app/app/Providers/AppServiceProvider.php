0cfa0f387da6581eaa51e5469bad78b6dc92aec2
Ris3TwO <ris3two@gmail.com>
2019-09-09 10:25:37 -0300

    Fix https

--- laravel-app/app/Providers/AppServiceProvider.php
+++ laravel-app/app/Providers/AppServiceProvider.php
@@ -21,8 +21,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        If(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
 }
